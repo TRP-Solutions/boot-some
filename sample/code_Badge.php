f0e4fb265092c76f85cd54ac71a31d04a50a735e
Jacob Neergaard Pedersen <neergaard@kananga.dk>
2019-04-07 20:48:12 +0200

    updated versions

--- sample/code_Badge.php
+++ sample/code_Badge.php
@@ -4,5 +4,5 @@ $main->el('h1')->te('Badge');
 
 $main->el('span')->te('Information')->badge();
 $main->el('br');
-$main->a('http://google.com/search?q=html5+elements')->te('Important!')->badge('danger');
+$main->a('http://google.com/search?q=important')->te('Important!')->badge('danger');
 ?>
--- sample/code_Index.php
+++ sample/code_Index.php
@@ -8,11 +8,11 @@ list($head,$body) = $doc->html('BootSome() :: '.$page);
 $head->metadata('viewport','width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no');
 $head->link('icon','data:,');
 
-$head->css('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
-$head->css('https://use.fontawesome.com/releases/v5.3.1/css/all.css');
+$head->css('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
+$head->css('https://use.fontawesome.com/releases/v5.8.1/css/all.css');
 
 $head->el('script',['src'=>'https://code.jquery.com/jquery-3.3.1.slim.min.js']);
-$head->el('script',['src'=>'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js']);
+$head->el('script',['src'=>'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js']);
 
 $head->css('../../git_boot-theme/lib/theme.css'); // https://github.com/TRP-Solutions/boot-theme
 ?>
